55d306788e7e4ead96a1614242f970416cd47319
avorycadoo <valerinerin24@gmail.com>
2024-02-22 14:30:54 +0700

    coba

---
+++ app/src/main/java/com/sally/anmp_week1/coba.kt
@@ -0,0 +1,11 @@
+package com.sally.anmp_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class coba : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coba)
+    }
+}
