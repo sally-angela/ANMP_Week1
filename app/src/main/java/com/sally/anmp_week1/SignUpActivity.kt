a7dcf6713b09ab8e793ff90df04e85968ef3678c
sally-angela <118360311+sally-angela@users.noreply.github.com>
2024-02-22 14:25:10 +0700

    Sign Up Activity

---
+++ app/src/main/java/com/sally/anmp_week1/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.sally.anmp_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
