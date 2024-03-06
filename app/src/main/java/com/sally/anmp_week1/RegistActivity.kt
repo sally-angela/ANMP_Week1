7c4af0615cc70a6212c18aed5bd2a7eb654b05bb
sally-angela <118360311+sally-angela@users.noreply.github.com>
2024-03-06 11:07:44 +0700

    .

--- app/src/main/java/com/sally/anmp_week1/RegistActivity.kt
+++ app/src/main/java/com/sally/anmp_week1/RegistActivity.kt
@@ -7,13 +7,11 @@ class RegistActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regist)
-<<<<<<< HEAD
-=======
+        
         //halo
         //aaaa
         //blasa
         //asdcaioda
             //sdiied
->>>>>>> 74bd6ba41a8430bb7aa5703dbe4ac3520523f0a4
     }
 }
